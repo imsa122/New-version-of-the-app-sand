479e8ba8ee6973fca46c5c921a2d14a34ae3a811
imsa122 <pccraft1133@hotmail.com>
2026-03-08 06:21:08 +0300

    ppo

--- Sanad/Services/FamilyLinkManager.swift
+++ Sanad/Services/FamilyLinkManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 final class FamilyLinkManager: ObservableObject {
     static let shared = FamilyLinkManager()
--- Sanad/Services/InactivityMonitor.swift
+++ Sanad/Services/InactivityMonitor.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Combine
 
 final class InactivityMonitor: ObservableObject {
     static let shared = InactivityMonitor()
